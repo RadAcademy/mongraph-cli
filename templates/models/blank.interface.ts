56022ca9547691f8a91bda6ad76e3bc3cd15057a
Ryann Galea <ryann@rad.academy>
2018-08-30 23:10:16 +1000

    updaes

--- templates/models/blank.interface.ts
+++ templates/models/blank.interface.ts
@@ -2,6 +2,8 @@ import * as mongoose from 'mongoose';
 
 export interface I{{name_c}} extends mongoose.Document {
   _id: string;
+  createdOn: string;
+  updatedOn: string;
   {{#each values}}
   {{this.name}}: {{this.type_interface}};
   {{/each}}
